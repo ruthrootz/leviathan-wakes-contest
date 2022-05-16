8f64e2004fe2014e828313d0efd352ee02bb8f0e
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-05-16 14:16:51 +0000

    cleanup

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-faction = input("Choose your faction [Earther, Belter, OPA, MCRN, Laconian, pirate (no affiliation)]:\n")
-ship = input("Choose your ship [Rocinante, Tycho, Razorback, Donnager, Gathering Storm]:\n")
+FACTIONS = ["earther", "belter", "opa", "mcrn", "laconian", "pirate"]
+SHIPS =  ["rocinante", "tycho", "razorback", "donnager", "gathering storm"]
+GATES = ["ilus", "laconia", "freehold", "auberon"]
+
+faction = input("Choose your faction {FACTIONS}:\n".format(FACTIONS = FACTIONS)).lower()
+ship = input("Choose your ship {SHIPS}:\n".format(SHIPS = SHIPS)).lower()
 
 if faction.lower() == 'earther':
-    print("We're the OG. That's why we're always right, even though our world is falling apart.")
-elif faction.lower() == "belter":
+    print(
+        "We're the OG. That's why we're always right, even though our world is falling apart."
+    )
+elif faction == "belter":
     print("Owkwa beltalowda!")
-elif faction.lower() == "opa":
+elif faction == "opa":
     print("Welcome, copeng. Let's get those inyalowda.")
-elif faction.lower() == "mcrn":
+elif faction == "mcrn":
     print("Donkey balls!")
-elif faction.lower() == "laconian":
-    print("Yay for being a powerful bad guy with strong but questionable values!")
-elif faction.lower() == "pirate  (no affiliation)":
-    print("You'll still get dragged into this mess.")
-elif ship not in ["Rocinante", "Tycho", "Razorback", "Donnager", "Gathering Storm"]:
+elif faction == "laconian":
+    print(
+        "Yay for being a powerful bad guy with strong but questionable values!"
+    )
+elif faction == "pirate":
+    print("You'll still get dragged into this mess, pirate, even if you don't want to fight for a cause.")
+if (ship not in SHIPS) | (faction not in FACTIONS):
     print("Your choices don't match the options... Try again.")
 
 # if pirate, choose what you smuggle, get captured
 # go to Ceres?
-    # get shot by drunk Miller
-    # win a xxx competition, Naomi likes you now (if on Roci)
-    # get drunk
+# get shot by drunk Miller
+# win a golgo competition, Naomi likes you now (if on Roci)
+# get drunk
 # leave Ceres?
-    # get drunk? & leave Ceres?
-        # you put [ship name] in manual and crash into an asteroid, idiot
-    # if not pirate, you leave Ceres and get chased by pirates
-    # if pirate, you get caputred and tortured by Laconians
+# get drunk? & leave Ceres?
+# you put [ship name] in manual and crash into an asteroid, idiot
+# if not pirate, you leave Ceres and get chased by pirates
+# if pirate, you get caputred and tortured by Laconians
 # go to Ganymede?
-    # get infected with the Protomolecule
+# get infected with the Protomolecule
 
 # "Still alive? Lucky. The universe isn't usually this nice."
 # "You head to the ring gate. Time to explore the universe!"
-    # if drunk, enter random ring gate
-    # else, choose a ring gate [Ilus, Laconia, Freehold, Auberon]
-        # if (OPA | pirate | rocinante) & auberon, get a contract "You get a contract to smuggle supplies to Freehold. Your career in the Underground has begun!"
-        # else, "You infultrate and capture a group of Underground members."
-        # if Laconia, get tortured by Laconians, say hi to the other mutants in the pit
-        # if freehold
-        # if ilus
+# if drunk, enter random ring gate
+# else, choose a ring gate
+# if (opa | pirate | rocinante) & auberon, get a contract "You get a contract to smuggle supplies to Freehold. Your career in the Underground has begun!"
+# else, "You infultrate and capture a group of Underground members."
+# if Laconia, "You get tortured by Laconians. Say hi to the other mutants in the pit!""
+# if freehold
+# if ilus
