90b273ee0d469ae6844c0ce3585e51fb831e05b9
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-05-16 14:20:04 +0000

    comment cleanup

--- main.py
+++ main.py
@@ -20,25 +20,25 @@ elif faction == "pirate":
 if (ship not in SHIPS) | (faction not in FACTIONS):
     print("Your choices don't match the options... Try again.")
 
-# if pirate, choose what you smuggle, get captured
+# if pirate, choose what you smuggle
 # go to Ceres?
-# get shot by drunk Miller
-# win a golgo competition, Naomi likes you now (if on Roci)
-# get drunk
+    # get shot by drunk Miller
+    # win a golgo competition, Naomi likes you now (if on Roci)
+    # get drunk
 # leave Ceres?
-# get drunk? & leave Ceres?
-# you put [ship name] in manual and crash into an asteroid, idiot
-# if not pirate, you leave Ceres and get chased by pirates
-# if pirate, you get caputred and tortured by Laconians
+    # got drunk?
+    # you put {ship} in manual and crash into an asteroid, idiot
+    # if not pirate, you leave Ceres and get chased by pirates
+    # if pirate, you get caputred and tortured by Laconians
 # go to Ganymede?
-# get infected with the Protomolecule
+    # get infected with the Protomolecule
 
 # "Still alive? Lucky. The universe isn't usually this nice."
 # "You head to the ring gate. Time to explore the universe!"
 # if drunk, enter random ring gate
 # else, choose a ring gate
-# if (opa | pirate | rocinante) & auberon, get a contract "You get a contract to smuggle supplies to Freehold. Your career in the Underground has begun!"
-# else, "You infultrate and capture a group of Underground members."
-# if Laconia, "You get tortured by Laconians. Say hi to the other mutants in the pit!""
-# if freehold
-# if ilus
+    # if (opa | pirate | rocinante) & auberon, get a contract "You get a contract to smuggle supplies to Freehold. Your career in the Underground has begun!"
+    # else, "You infultrate and capture a group of Underground members."
+    # if Laconia, "You get tortured by Laconians. Say hi to the other mutants in the pit!""
+    # if freehold
+    # if ilus
